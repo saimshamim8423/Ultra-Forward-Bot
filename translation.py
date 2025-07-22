5f6c0102adb49b15072df827a8a0664c3367ff76
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2025-07-22 12:27:06 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -19,7 +19,7 @@ class Translation(object):
   
 ➻ Click Help Button To Know More About Me
   
-<b>Bot Is Made By @Madflix_Bots</b>"""
+<b>Bot Is Made By @beesmileee</b>"""
 
 
   HELP_TXT = """<b><u>🛠️ Help</b></u>
@@ -54,10 +54,10 @@ class Translation(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📢 Channel :</b> <a href='https://t.me/Anity_managementbots'>Anity Botz</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/beesmileee'>Smile Developer</a>
 
-<b>♻️ Bot Made By :</b> @Madflix_Bots"""
+<b>♻️ Bot Made By :</b> @Anity_managementbots"""
   
   STATUS_TXT = """<b><u>Bot Status</u></b>
   
